d4dea3b79c7b05844b768a1ada0d6fca1a5ab785
pravesh meena <pravesh.meena2892@gmail.com>
2024-03-01 21:49:28 +0530

    final update

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 let currentStep = 1;
 let userData = {};
 
+function startOnboarding() {
+    const welcomeContainer = document.getElementById('welcome-container');
+    const onboardingContainer = document.getElementById('onboarding-container');
+
+    welcomeContainer.style.display = 'none';
+    onboardingContainer.style.display = 'flex';
+}
+
 function nextStep() {
     const currentStepElement = document.getElementById(`step${currentStep}`);
-
    
     if (currentStep === 1) {
         const userNameInput = document.getElementById('user-name');
         const userEmailInput = document.getElementById('user-email');
-
+        const userFitnessGoal = document.querySelector('input[name="fitnessGoal"]:checked');
+        const userExerciseLevel = document.querySelector('input[name="ExerciseLevel"]:checked');
         const userName = userNameInput.value;
         const userEmail = userEmailInput.value;
 
-       
         userNameInput.style.border = '';
         userEmailInput.style.border = '';
 
-        if (!userName || !userEmail) {
-            
+        if (!userName || !userEmail || !userFitnessGoal || !userExerciseLevel) {
             if (!userName) {
                 userNameInput.style.border = '2px solid red';
             }
@@ -26,6 +32,30 @@ function nextStep() {
             }
             return; 
         }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(userEmail)) {
+            userEmailInput.style.border = '2px solid red';
+            return;
+        }
+
+        userData['name'] = userName;
+        userData['email'] = userEmail;
+        userData['fitnessGoal'] = userFitnessGoal.value;
+        userData['exerciseLevel'] = userExerciseLevel.value;
+    }
+
+    if (currentStep === 2) {
+        userData['exerciseTypes'] = [];
+        const checkboxes = document.querySelectorAll('input[name="exercise"]:checked');
+        checkboxes.forEach((checkbox) => {
+            userData['exerciseTypes'].push(checkbox.value);
+        });
+
+        const workoutDuration = document.querySelector('input[name="duration"]:checked');
+        const preferredTime = document.querySelector('input[name="time"]:checked');
+
+        userData['workoutDuration'] = workoutDuration ? workoutDuration.value : '';
+        userData['preferredTime'] = preferredTime ? preferredTime.value : '';
     }
 
     currentStepElement.classList.remove('active-step');
@@ -44,17 +74,12 @@ function nextStep() {
 
 function submitForm() {
  
-    userData['name'] = document.getElementById('user-name').value;
-    userData['email'] = document.getElementById('user-email').value;
+    userData['age'] = document.getElementById('user-age').value;
+    userData['weight'] = document.getElementById('user-weight').value;
+    userData['height'] = document.getElementById('user-Height').value;
 
-   
-    userData['exerciseTypes'] = [];
-    const checkboxes = document.querySelectorAll('input[name="exercise"]:checked');
-    checkboxes.forEach((checkbox) => {
-        userData['exerciseTypes'].push(checkbox.value);
-    });
-
-    userData['weightGoals'] = document.getElementById('weight-goals').value;
+    const weightGoal = document.querySelector('input[name="weightGoal"]:checked');
+    userData['weightGoal'] = weightGoal ? weightGoal.value : '';
 
     const jsonData = JSON.stringify(userData);
     const blob = new Blob([jsonData], { type: 'application/json' });
@@ -62,14 +87,22 @@ function submitForm() {
     link.href = window.URL.createObjectURL(blob);
     link.download = 'user_data.json';
     link.click();
-
+  
     const confirmationMessage = document.getElementById('confirmation-message');
-    confirmationMessage.innerHTML = '<p>Thank you for providing your information!</p>';
-    confirmationMessage.style.display = 'block';
+    if (window.innerWidth > 600) {
+        confirmationMessage.innerHTML = '<p>Congratulations! Your personalized fitness plan is ready to go.</p>';
+        confirmationMessage.style.display = 'block';
+    } else {
+      
+        confirmationMessage.style.display = 'none';
+    }
+
+    
 }
 
+
 function typeEffect() {
-    var text = "Welcome to our Fitness Service!";
+    var text = "Welcome to our Innovative Fitness";
     var header = document.getElementById("header-text");
     var index = 0;
 
